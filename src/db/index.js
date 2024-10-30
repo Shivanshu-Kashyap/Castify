e3b0478a74a24800bf5312b60bd0dda28ce5afd9
Shivanshu-Kashyap <shivanshukashyap996@gmail.com>
2024-10-30 20:48:45 +0530

    postman learning

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,18 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB = async() =>{
+const connectDB = async () => {
     try {
-      const connectionInstance =   await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-      console.log(`\n Mongodb connected !! DB HOST: ${connectionInstance}`);
+        console.log(`\n MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MONGODB connection error:", error);
-        process.exit(1)
+        console.log("MongoDB connection error:", error);
+        process.exit(1);
     }
-    
-}
+};
 
-export default connectDB
+export default connectDB;

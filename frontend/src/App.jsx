a46ef7007f5f05fb76b1fd25aa8623fb8e3feff9
Shivanshu-Kashyap <shivanshukashyap996@gmail.com>
2024-11-04 11:54:02 +0530

    configure frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+
+   Hello let's create a fromntend for our backend material; let's goo
+   </>
+  )
+}
+
+export default App
